66dcc5c9602ad7b125312ef29332a0fceafa7a78
RhysTalley <145939008+RhysTalley@users.noreply.github.com>
2024-11-22 17:21:31 -0500

    Added cors to app.py

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from config import Config
 from models import db
 from routes import bp
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
 db.init_app(app)
